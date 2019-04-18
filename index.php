e3482c3cacfd54014116b8609d6517cfeb87e631
Натали <t.natalie14@gmail.com>
2019-04-18 13:02:12 +0400

    поправила названия переменных в циклах перебора массивов

--- index.php
+++ index.php
@@ -110,9 +110,9 @@ $lots = [
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($categories as $category): ?>
+            <?php foreach ($categories as $category => $alias): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= $category; ?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?= $alias ?></a>
             </li>
             <?php endforeach; ?>
         </ul>
@@ -123,18 +123,18 @@ $lots = [
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <?php foreach ($lots as $lots_key => $lots_val): ?>
+            <?php foreach ($lots as $lot): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$lots_val["path"]; ?>" width="350" height="260" alt="">
+                    <img src="<?=$lot["path"]; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$lots_val["category"]; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lots_val["name"]; ?></a></h3>
+                    <span class="lot__category"><?=$lot["category"]; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot["name"]; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$lots_val["price"]; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$lot["price"]; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -152,9 +152,9 @@ $lots = [
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($categories as $category): ?>
+            <?php foreach ($categories as $category => $alias): ?>
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?= $category; ?></a>
+                <a href="pages/all-lots.html"><?= $alias; ?></a>
             </li>
             <?php endforeach; ?>
         </ul>
