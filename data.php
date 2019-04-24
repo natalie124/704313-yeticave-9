dfccb729ef06afa8ce67f5bee2c3e6ffff54afcd
Натали <t.natalie14@gmail.com>
2019-04-24 15:27:34 +0400

    добавила счетчик времени для лота

--- data.php
+++ data.php
@@ -19,41 +19,35 @@ $lots = [
         "category" => $categories["boards"],
         "price" => 10999,
         "path" => "img/lot-1.jpg"
-
     ],
     [
         "name" => "DC Ply Mens 2016/2017 Snowboard",
         "category" => $categories["boards"],
         "price" => 159999,
         "path" => "img/lot-3.jpg"
-
     ],
     [
         "name" => "Крепления Union Contact Pro 2015 года размер L/XL",
         "category" => $categories["attachment"],
         "price" => 8000,
         "path" => "img/lot-3.jpg"
-
     ],
     [
         "name" => "Ботинки для сноуборда DC Mutiny Charocal",
         "category" => $categories["boards"],
         "price" => 10999,
         "path" => "img/lot-4.jpg"
-
     ],
     [
         "name" => "Куртка для сноуборда DC Mutiny Charocal",
         "category" => $categories["boots"],
         "price" =>7500,
         "path" => "img/lot-5.jpg"
-
     ],
     [
         "name" => "Маска Oakley Canopy",
         "category" => $categories["other"],
         "price" => 5400,
         "path" => "img/lot-6.jpg"
-
     ]
 ];
