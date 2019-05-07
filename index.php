f5060a76696432641598b62379f187ed1ddf56ec
Натали <t.natalie14@gmail.com>
2019-05-07 11:39:39 +0400

    перенесла запросы лотов и категорий в index.php из data.php

--- index.php
+++ index.php
@@ -5,6 +5,14 @@ require_once('data.php');
 require_once('functions.php');
 require_once('init.php');
 
+$sql_cat = "SELECT id, name, symbol_code FROM categories"; // получаем все категрии
+
+$sql_lots = "SELECT l.title AS name, l.price AS start_price, l.img_path, c.name AS category FROM lots AS l
+            LEFT JOIN categories AS c ON l.cat_id = c.id
+            WHERE NOW() < l.dt_end AND l.win_id IS NULL
+            ORDER BY l.dt_add DESC
+            LIMIT 9"; // получаем самые новые открытые лоты (каждый лот включает название, стартовую цену, ссылку на изображение, название категории)
+
 $categories = get_rows_from_mysql($con, $sql_cat);
 $lots = get_rows_from_mysql($con, $sql_lots);
 
